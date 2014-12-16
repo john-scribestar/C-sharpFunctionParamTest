e3abc45d8077560f919fcc690b657091afb2bdb6
john-scribestar <john.harris@scribestar.com>
2014-12-16 11:04:36 +0000

    Adding project to repo

---
+++ CSharpfunctionParamTest/CSharpfunctionParamTest/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CSharpfunctionParamTest
+{
+  using System;
+  using System.Drawing;
+  using System.Drawing.Imaging;
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      CreateAndTestfunction();
+    }
+
+    public static void CreateAndTestfunction()
+    {
+      var b = new Bitmap(500, 500, PixelFormat.Format32bppArgb);
+      MethodAcecptingtestFunction(() => Testfunction(b));
+    }
+
+    public static void MethodAcecptingtestFunction(Func<Bitmap> testFunc)
+    {
+      using (var b = testFunc())
+      {
+        Console.Write(b);
+      }
+    }
+
+    public static Bitmap Testfunction(Bitmap b)
+    {
+      return b;
+    }
+  }
+}
